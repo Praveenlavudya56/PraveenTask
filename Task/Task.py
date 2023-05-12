7a857b859e8257144c8017f902c9c6277bc8a48d
unknown <praveenlavudya56@gmail.com>
2023-05-12 11:12:46 +0530

    i praveen Task 12-05-2023

---
+++ Task/Task.py
@@ -0,0 +1,40 @@
+# print largest of the three numbers using only IF Stetment
+
+# x=int(input("Entar the Number: "))
+# y=int(input("Entar the Number: "))
+# z=int(input("Entar the Number: "))
+
+# if x>y and x>z:
+#     print("largest number is:",x)
+# elif y>x and y>z:
+#     print("largest number is:",y)
+# else :
+#     print("largest number is:",z)
+
+
+
+#check whether a number is even or not ?
+
+# num=int(input("Enter the Number"))
+# if num %2==0:
+#         print("Given Number is Even")
+# else:
+#         print("This is Not Even Number Thank you")
+
+#.program to check student grades ??
+marks = float(input("Enter you are marks: "))
+
+if marks >=90:
+    print("your grade is : A")
+elif marks >=80 and marks <90:
+    print("Your grade is : B ")
+elif marks >=70 and marks <80:
+    print("your grade is : C ")
+elif marks >=60 and marks <70:
+    print("your grade is : D ")
+elif marks >=50 and marks <60:
+    print("you are grade is : E ")
+elif marks >=40 and marks <50:
+    print("you are grade is: F ")
+else:
+    print("sorry your Faile betar luck next time ")
